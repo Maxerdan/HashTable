8a2ef24512bbe022acec6efa3f44e39150875151
Maxerdan <48237187+Maxerdan@users.noreply.github.com>
2019-03-26 18:44:19 +0500

    space

--- HashTable/HashTable.cs
+++ HashTable/HashTable.cs
@@ -8,7 +8,7 @@ namespace HashTable
         public object Key { get; set; }
         public object Value { get; set; }
         public KeyValuePair(object key, object value)
-        {
+        { 
             Key = key;
             Value = value;
         }
